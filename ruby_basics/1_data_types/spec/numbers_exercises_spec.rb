b323a1cc43ac82847bcc44ab48e4eeca15fe5961
River Fisher <river.bass.fisher@gmail.com>
2026-07-17 15:39:55 -0400

    Lesson 1: Learned that spec has a eq and an eql comparer. eql is stricter and matches type. Tests passed even though I did not return an integer but a float.

--- ruby_basics/1_data_types/spec/numbers_exercises_spec.rb
+++ ruby_basics/1_data_types/spec/numbers_exercises_spec.rb
@@ -57,11 +57,11 @@ RSpec.describe 'Numbers Exercises' do
   describe 'string to number exercise' do
 
     it 'returns an integer from a string' do
-      expect(string_to_number('1')).to eq(1)
+      expect(string_to_number('1')).to eql(1)
     end
 
     it 'returns a negative integer from a string' do
-      expect(string_to_number('-5')).to eq(-5)
+      expect(string_to_number('-5')).to eql(-5)
     end
   end
 
